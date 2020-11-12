648ccda0bb4fbfc5303cd1b60b39bcea290b7131
Ванівський Назар <31289466+NazarK0@users.noreply.github.com>
2020-11-12 11:50:13 +0200

    Initial commit

---
+++ app/components/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Home.scss';
+
+export default function Home(): JSX.Element {
+  return (
+    <div className={styles.container} data-tid="container">
+      <h2>Home test</h2>
+    </div>
+  );
+}
